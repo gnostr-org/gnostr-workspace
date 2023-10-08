472e819cca94b3499b055271129f699554cf00ca
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-10-08 15:06:35 -0400

    make:cargo

--- cargo.mk
+++ cargo.mk
@@ -1,34 +1,47 @@
-##
-##make cargo-*
-cargo-help:### 	cargo-help
-	@awk 'BEGIN {FS = ":.*?###"} /^[a-zA-Z_-]+:.*?###/ {printf "\033[36m%-15s\033[0m %s\n", $$1, $$2}' $(MAKEFILE_LIST)
-cargo-b:cargo-build###	cargo b
-cargo-build:###	cargo build
-## 	make cargo-build q=true
+cargo:## 	cargo  commands
+#                          cargo                    cargo
+	@awk 'BEGIN {FS = ":.*?#####	"} /^[a-zA-Z_-]+:.*?#####	/ {printf "\033[36m%-15s\033[0m %s\n", $$1, $$2}' $(MAKEFILE_LIST)
+
+cargo-build:#####	cargo-build
+##make cargo-build && ./target/debug/gnostr-sha256 gr README.md
+##make cargo-build && ./target/debug/gnostr-sha256
+	@type -P rustup && rustup default nightly
 	@. $(HOME)/.cargo/env
-	@RUST_BACKTRACE=all cargo b $(QUIET)
-## cargo-install:### 	cargo install --path .
-## #@. $(HOME)/.cargo/env
-## 	#@cargo install --path $(PWD)
-## 	@cargo install --locked --path $(PWD)
-cargo-br:cargo-build-release###	cargo-br
-## 	make cargo-br q=true
-cargo-build-release:###	cargo-build-release
-## 	make cargo-build-release q=true
+	@echo cargo b
+	@cargo b
+cargo-install:#####	cargo-install
+	@. $(HOME)/.cargo/env>/tmp/gnostr-command.log
+#@echo cargo install --path $(PWD)/deps/gnostr-*
+#@cargo install --path $(PWD)/deps/gnostr-*
+#@echo "export PATH=$(CARGO_PATH)/bin:$(PATH)"
+cargo-check:#####	cargo-check
 	@. $(HOME)/.cargo/env
-	@cargo b --release $(QUIET)
-cargo-check:###	cargo-check
-	@. $(HOME)/.cargo/env
+	@echo cargo c
 	@cargo c
-cargo-bench:###	cargo-bench
+cargo-bench:#####	cargo-bench
 	@. $(HOME)/.cargo/env
+	@echo cargo bench
 	@cargo bench
-cargo-test:###	cargo-test
+cargo-clean:#####	cargo-clean
 	@. $(HOME)/.cargo/env
+	@echo cargo clean
+	@cargo clean
+cargo-test:#####	cargo-test
+	@. $(HOME)/.cargo/env
+	@echo cargo test
 	@cargo test
-cargo-report:###	cargo-report
+cargo-build-release:#####	cargo-build-release
+##make cargo-build-release && ./target/release/gnostr-sha256 gr README.md
+##make cargo-build-release && ./target/release/gnostr-sha256
+	@type -P rustup && rustup default nightly
+	@. $(HOME)/.cargo/env>/tmp/gnostr-cli.log
+	#@echo cargo b --release
+	@cargo b --release
+cargo-publish:#####	cargo-publish
+	@type -P rustup && rustup default nightly
 	@. $(HOME)/.cargo/env
-	cargo report future-incompatibilities --id 1
+	@echo cargo publish
+	@cargo publish
 
 # vim: set noexpandtab:
 # vim: set setfiletype make
